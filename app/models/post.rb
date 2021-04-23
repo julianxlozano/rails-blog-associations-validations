f8527a890d16a0a3cdde578d2998ba74ab8c526f
julianxlozano <julianxlozano@gmail.com>
2021-04-23 16:27:50 -0500

    commit tree 097e6b9da668408ae42686a31799fc53a73b65b6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,19 @@
 class Post < ActiveRecord::Base
-  
+    has_many :post_tags
+    has_many :tags, through: :post_tags
+    belongs_to :user 
+    accepts_nested_attributes_for :tags
+
+    validates :name, presence: true 
+    validates :content, presence: true
+
+    
+  def tags_attributes=(tag_attributes)
+    tag_attributes.values.each do |tag_attribute|
+      tag = Tag.find_or_create_by(tag_attribute)
+      self.tags << tag
+    end
+  end
+
+
 end
